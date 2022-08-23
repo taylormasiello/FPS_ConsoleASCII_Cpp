2bb0124e5ed05459fc4c05113db54490c510e646
Taylor Masiello <masiellotay@gmail.com>
2022-08-22 20:53:58 -0700

    Add project files.

---
+++ FPS_ConsoleASCII/FPS_ConsoleASCII.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//outpus to console, very slow
+	for (int i = 0; i < 10000; i++)
+	{
+		cout << "Hello!" << i << endl;
+	}
+
+	return 0;
+}
+
+
